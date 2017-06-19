536d1d6e831290f987b5ebb3396fb63e759afdd4
Sandeshgangadhar <sandeshgangadhar@outlook.com>
2017-06-19 21:54:47 +0530

    ProgrammingAssignment2 v1

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,73 @@
 ## Calculates an inverse of the matrix and caches the result. 
 #  So that the next time when the function is called it checks if the output is already available
-#  If "yes" it outputs cached result or calculates the inverse of the matrix 
-## functions do
+#  If "yes" it outputs cached result or else it calculates the inverse of the matrix and outputs that
 
-## 
 
+
+## Receives a matrix as input and functions to calculate inverse of a matrix
+#  / returns input and solved matrix
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  
+  set <- function(y){ #To input a matrix
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x    # Returns the input matrix
+    
+  setsolve <- function(solve)  m <<- solve #Inverse of the matrix is calculated 
+   
+  getsolve <- function() m # Returns inverse of a matrix
 
+  #Output list containing Input/Inverse matrix and Set/Solve Input matrix functions
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## makeCacheMatrix object is provided as an input argument
+## Verifies if the matrix is already solved/not solved
+
+## Outputs the result by calculating if already not solved or 
+#  just retrieves and outputs the result if already solved .
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getsolve() # Returns inverse matrix if already found else 
+                      # NULL is returned and inverse is yet to be calculated
+    
+    if(!is.null(m)) { # Verifies if already solved or not
+        message("Retrieving cached data")
+        return(m) #Returns cached output // and program terminates here if cached output is found
+    }
+    data <- x$get() #Retreive input matrix
+    
+    m <- solve(data, ...) #solve function is initiated for the input data(matrix)
+    
+    x$setsolve(m) #setsolve function of makeCacheMatrix object is called and inverse of the matrix is calculated
+    
+    m #Returns inverse of a matrix and program terminates here if cached output is not available
+        
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
